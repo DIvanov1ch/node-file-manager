944a310a859ff8862768b70156a506a2a1ac49dd
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-03 20:23:57 +0300

    feat: implement deleteFile function

---
+++ src/operations/deleteFile.js
@@ -0,0 +1,23 @@
+import { rm } from "node:fs/promises";
+import { resolve } from "node:path";
+
+import { Path } from "../Path.js";
+import { getOperationFailedMessage } from "../messages.js";
+
+export const deleteFile = async (path) => {
+  const fileToRemovePath = resolve(Path.getCurrentPath(), path);
+  try {
+    await rm(fileToRemovePath);
+    console.log(`'${fileToRemovePath}' successfully removed!`);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.log(
+        getOperationFailedMessage(
+          `No such file or directory: '${fileToRemovePath}'`
+        )
+      );
+    } else {
+      console.log(getOperationFailedMessage(error.message));
+    }
+  }
+};
