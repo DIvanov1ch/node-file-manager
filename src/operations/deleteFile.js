49550c4ca218c8b8ee043b3173604b3821564f24
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-06 02:43:36 +0300

    refactor: update deleteFile()

--- src/operations/deleteFile.js
+++ src/operations/deleteFile.js
@@ -2,22 +2,13 @@ import { rm } from "node:fs/promises";
 import { resolve } from "node:path";
 
 import { Path } from "../Path.js";
-import { getOperationFailedMessage } from "../messages.js";
+import { printError } from "../utils/printError.js";
 
 export const deleteFile = async (path) => {
   const fileToRemovePath = resolve(Path.getCurrentPath(), path);
   try {
-    await rm(fileToRemovePath, { force: true });
-    console.log(`'${fileToRemovePath}' successfully removed!`);
+    await rm(fileToRemovePath);
   } catch (error) {
-    if (error.code === "ENOENT") {
-      console.log(
-        getOperationFailedMessage(
-          `No such file or directory: '${fileToRemovePath}'`
-        )
-      );
-    } else {
-      console.log(getOperationFailedMessage(error.message));
-    }
+    printError(error);
   }
 };
