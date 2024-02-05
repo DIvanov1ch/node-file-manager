79341e4d70aba62c67569de910f656d37bf228b3
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-06 02:32:51 +0300

    refactor: update rename()

--- src/operations/renameFile.js
+++ src/operations/renameFile.js
@@ -3,11 +3,9 @@ import { rename } from "node:fs/promises";
 import { existsSync } from "node:fs";
 
 import { Path } from "../Path.js";
-import { extractSourceAndDestination as parsePath } from "../utils/extractSrcAndDest.js";
-import {
-  getInvalidArgsMessage,
-  getOperationFailedMessage,
-} from "../messages.js";
+import { extractPaths as parsePath } from "../utils/extractPaths.js";
+import { getInvalidArgsMessage } from "../messages.js";
+import { printError } from "../utils/printError.js";
 
 export const renameFile = async (paths) => {
   const [oldFilename, newFilename] = parsePath(paths);
@@ -19,25 +17,12 @@ export const renameFile = async (paths) => {
   const newPath = join(dirname(oldPath), newFilename);
   try {
     if (existsSync(newPath)) {
-      const existError = new Error("File already exists");
+      const existError = new Error(`File already exists ${newPath}`);
       existError.code = "EEXIST";
       throw existError;
     }
     await rename(oldPath, newPath);
-    console.log("File successfully renamed!");
   } catch (error) {
-    if (error.code === "ENOENT") {
-      console.log(
-        getOperationFailedMessage(
-          `No such file or directory: '${oldPath}' -> '${newPath}'`
-        )
-      );
-    } else if (error.code === "EEXIST") {
-      console.log(
-        getOperationFailedMessage(`File already exists: '${newPath}'`)
-      );
-    } else {
-      console.log(getOperationFailedMessage(error.message));
-    }
+    printError(error);
   }
 };
