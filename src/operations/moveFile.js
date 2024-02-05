86e6aaf94553d34a24f61d8652560d90add1b166
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-06 02:42:18 +0300

    refactor: update moveFile()

--- src/operations/moveFile.js
+++ src/operations/moveFile.js
@@ -1,14 +1,13 @@
-import { resolve, basename } from "node:path";
+import { resolve, posix, win32 } from "node:path";
 import { createReadStream, createWriteStream } from "node:fs";
 import { pipeline } from "node:stream/promises";
 import { rm } from "node:fs/promises";
 
 import { Path } from "../Path.js";
-import { extractSourceAndDestination as parsePath } from "../utils/extractSrcAndDest.js";
-import {
-  getInvalidArgsMessage,
-  getOperationFailedMessage,
-} from "../messages.js";
+import { isWindows } from "../constants.js";
+import { extractPaths as parsePath } from "../utils/extractPaths.js";
+import { getInvalidArgsMessage } from "../messages.js";
+import { printError } from "../utils/printError.js";
 
 export const moveFile = async (paths) => {
   const [src, dest] = parsePath(paths);
@@ -17,26 +16,14 @@ export const moveFile = async (paths) => {
     return;
   }
   const source = resolve(Path.getCurrentPath(), src);
-  const destination = resolve(Path.getCurrentPath(), dest, basename(source));
+  const filename = isWindows ? win32.basename(source) : posix.basename(source);
+  const destination = resolve(Path.getCurrentPath(), dest, filename);
   try {
     const input = createReadStream(source);
     const output = createWriteStream(destination, { flags: "wx" });
     await pipeline(input, output, { end: false });
     await rm(source, { force: true });
-    console.log("File successfully moved!");
   } catch (error) {
-    if (error.code === "ENOENT") {
-      console.log(
-        getOperationFailedMessage(
-          `No such file or directory: '${source}' -> '${destination}'`
-        )
-      );
-    } else if (error.code === "EEXIST") {
-      console.log(
-        getOperationFailedMessage(`File already exists: '${destination}'`)
-      );
-    } else {
-      console.log(getOperationFailedMessage(error.message));
-    }
+    printError(error);
   }
 };
